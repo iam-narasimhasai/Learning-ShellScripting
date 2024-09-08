d9d126f8e82efab657e746dc524689bb66c8e653
iam-narasimhasai <narasimhasai.nimmagadda@gmail.com>
2024-09-08 23:56:16 +0530

    forloop.sh

---
+++ forloop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+
+echo "number is $i"
+
+done
+
+for j in {1..5}
+do
+
+echo "number is $(($j*$j))"
+
+done
+
+for name in rao sai ramu
+do
+
+echo "$name"
+
+done
